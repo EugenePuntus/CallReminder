d979fea7828d9281ce632846cdb3fbf84f2f989e
Yauheni Puntus <Yauheni_Puntus@epam.com>
2019-03-07 12:31:54 +0300

    refactoring

--- CallReminder.Common/Class1.cs
+++ CallReminder.Common/Class1.cs
@@ -4,19 +4,5 @@ namespace CallReminder.Common
 {
     public class Class1
     {
-        private int HotFix()
-        {
-            return 3;
-        }
-
-        private void Issue03()
-        {
-            Console.WriteLine("Do work...");
-        }
-
-        public void ParIssue03()
-        {
-            Issue03();
-        }
     }
 }
