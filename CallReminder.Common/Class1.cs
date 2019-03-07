37585629f561744dd3dee3ce4fd4e4ce982703be
Yauheni Puntus <Yauheni_Puntus@epam.com>
2019-03-07 12:26:14 +0300

    finished issue-03

--- CallReminder.Common/Class1.cs
+++ CallReminder.Common/Class1.cs
@@ -4,9 +4,14 @@ namespace CallReminder.Common
 {
     public class Class1
     {
-        public void Issue03()
+        private void Issue03()
         {
             Console.WriteLine("Do work...");
+        }
+
+        public void ParIssue03()
+        {
+            Issue03();
         }
     }
 }
