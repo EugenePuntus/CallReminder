6122d9c118f476b318de660c8a26705a3d9c92ac
Yauheni Puntus <Yauheni_Puntus@epam.com>
2019-03-25 10:25:38 +0300

    removed excess files

--- CallReminder.Droid/ValueConverters/PhotoIdToPhotoUriValueConverter.cs
+++ CallReminder.Droid/ValueConverters/PhotoIdToPhotoUriValueConverter.cs
@@ -1,28 +1,10 @@
 ﻿using System;
 using System.Globalization;
-using Android.Content;
-using Android.Provider;
 using FlexiMvvm.ValueConverters;
 using Uri = Android.Net.Uri;
 
 namespace CallReminder.Droid.ValueConverters
 {
-
-    internal class PhotoFileIdToUriValueConverter : ValueConverter<long, Uri>
-    {
-        protected override ConversionResult<Uri> Convert(long value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value == 0)
-            {
-                return null;
-            }
-
-            var contactUri = ContentUris.WithAppendedId(ContactsContract.DisplayPhoto.ContentUri, value);
-
-            return ConversionResult<Uri>.SetValue(contactUri);
-        }
-    }
-
     internal class StringToUriValueConverter : ValueConverter<string, Uri>
     {
         protected override ConversionResult<Uri> Convert(string value, Type targetType, object parameter, CultureInfo culture)
